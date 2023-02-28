fb94b144b7cf570bdc02b389ac2ad7aaa7c53889
eternitytears <72160807+eternitytears@users.noreply.github.com>
2023-02-28 08:50:00 +0300

    Add files via upload

---
+++ game-logic.js
@@ -0,0 +1,100 @@
+
+var io
+var gameSocket
+var gamesInSession = []
+
+
+const initializeGame = (sio, socket) => {
+
+    io = sio 
+    gameSocket = socket 
+
+    // pushes this socket to an array which stores all the active sockets.
+    gamesInSession.push(gameSocket)
+
+    // Run code when the client disconnects from their socket session. 
+    gameSocket.on("disconnect", onDisconnect)
+
+    // Sends new move to the other socket session in the same room. 
+    gameSocket.on("new move", newMove)
+
+    // User creates new game room after clicking 'submit' on the frontend
+    gameSocket.on("createNewGame", createNewGame)
+
+    // User joins gameRoom after going to a URL with '/game/:gameId' 
+    gameSocket.on("playerJoinGame", playerJoinsGame)
+
+    gameSocket.on('request username', requestUserName)
+
+    gameSocket.on('recieved userName', recievedUserName)
+
+}
+
+
+function playerJoinsGame(idData) {
+    // A reference to the player's Socket.IO socket object
+    var sock = this
+    
+    // Look up the room ID in the Socket.IO manager object.
+    var room = io.sockets.adapter.rooms[idData.gameId]
+   // console.log(room)
+
+    // If the room exists...
+    if (room === undefined) {
+        this.emit('status' , "This game session does not exist." );
+        return
+    }
+    if (room.length < 2) {
+        // attach the socket id to the data object.
+        idData.mySocketId = sock.id;
+
+        // Join the room
+        sock.join(idData.gameId);
+
+        console.log(room.length)
+
+        if (room.length === 2) {
+            io.sockets.in(idData.gameId).emit('start game', idData.userName)
+        }
+
+        // Emit an event notifying the clients that the player has joined the room.
+        io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);
+
+    } else {
+        // Otherwise, send an error message back to the player.
+        this.emit('status' , "There are already 2 people playing in this room." );
+    }
+}
+
+
+function createNewGame(gameId) {
+    // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
+    this.emit('createNewGame', {gameId: gameId, mySocketId: this.id});
+
+    // Join the Room and wait for the other player
+    this.join(gameId)
+}
+
+
+function newMove(move) {
+    const gameId = move.gameId
+    
+    io.to(gameId).emit('opponent move', move);
+}
+
+function onDisconnect() {
+    var i = gamesInSession.indexOf(gameSocket);
+    gamesInSession.splice(i, 1);
+}
+
+
+function requestUserName(gameId) {
+    io.to(gameId).emit('give userName', this.id);
+}
+
+function recievedUserName(data) {
+    data.socketId = this.id
+    io.to(data.gameId).emit('get Opponent UserName', data);
+}
+
+exports.initializeGame = initializeGame

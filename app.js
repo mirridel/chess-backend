5eba287fe2c189a1b3ed0c2d0222c83cfc0a1fc9
mirridel <71335429+mirridel@users.noreply.github.com>
2023-03-04 14:35:42 +0700

    v0.01

--- app.js
+++ app.js
@@ -2,15 +2,136 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const gameLogic = require('./game-logic')
+const crypto = require("crypto");
 const app = express()
-
 
 const server = http.createServer(app)
 const io = socketio(server)
+const PORT = 8000
+
+const pgp = require('pg-promise')(/* options */)
+db = pgp('postgres://postgres:19092001@localhost/Chess')
+
+function password_hashing(input_data) {
+    const secret = "novosibirsk";
+    const hasher = crypto.createHmac("sha256", secret);
+    return hasher.update(input_data).digest("hex");
+}
 
 io.on('connection', client => {
-    gameLogic.initializeGame(io, client)
+    /**
+     * РЕГИСТРАЦИЯ
+     * Входные данные (input_data):
+     * {'username': <username>, 'password': <password>, 'secret_question': <secret_question>, 'answer': <answer>}
+     * Ответ (успех):
+     * {'id': <id>}
+     * Ответ (ошибка):
+     * {'id': -1}
+     */
+    client.on('reg', registration_handler);
+
+    function registration_handler(input_data) {
+        try {
+            if (input_data['username'] !== '' && input_data['username'] !== null &&
+                input_data['password'] !== '' && input_data['password'] !== null &&
+                input_data['secret_question'] !== '' && input_data['secret_question'] !== null &&
+                input_data['answer'] !== '' && input_data['answer'] !== null) {
+                db.tx(t => {
+                    input_data['hashed_password'] = password_hashing(input_data['password']);
+                    const query = 'INSERT INTO users(username, password, secret_question, answer)' +
+                        'VALUES(${username}, ${hashed_password}, ${secret_question}, ${answer}) RETURNING id'
+                    return t.one(query, input_data);
+                })
+                    .then(data => {
+                        // COMMIT
+                        client.emit("reg", {"id": data.id})
+                    })
+                    .catch(error => {
+                        // ROLLBACK
+                        client.emit("reg", {"id": -1})
+                    });
+            }
+        } catch (e) {
+            // console.error(e)
+        }
+    }
+
+    /**
+     * АВТОРИЗАЦИЯ
+     * Входные данные (input_data):
+     * {'username': <username>, 'password': <password>}
+     * Ответ (успех):
+     * {'id': <id>, 'username': <username>}
+     * Ответ (ошибка):
+     * {'id': -1}
+     */
+    client.on('auth', authorization_handler);
+
+    function authorization_handler(input_data) {
+        try {
+            if (input_data['username'] !== '' && input_data['username'] !== null &&
+                input_data['password'] !== '' && input_data['password'] !== null) {
+                db.tx(t => {
+                    input_data['hashed_password'] = password_hashing(input_data['password']);
+                    const query = 'SELECT * FROM users WHERE username = ${username} AND password = ${hashed_password}';
+                    return t.one(query, input_data);
+                })
+                    .then(data => {
+                        // COMMIT
+                        client.emit("auth", {"id": data.id, "username": data.username})
+                        gameLogic.initializeGame(io, client, db)
+                    })
+                    .catch(error => {
+                        // ROLLBACK
+                        client.emit("auth", {"id": -1})
+                    });
+            }
+        } catch (e) { }
+    }
+
+    client.on('get_secret_question', get_secret_question_handler);
+
+    function get_secret_question_handler(input_data) {
+        try {
+            db.tx(t => {
+                const query = 'SELECT * FROM users WHERE username = ${username}';
+                return t.one(query, input_data);
+            })
+                .then(data => {
+                    // COMMIT
+                    client.emit("get_secret_question", {"username": data.username, "secret_question": data.secret_question})
+                })
+                .catch(error => {
+                    // ROLLBACK
+                    console.log(error)
+                    client.emit("get_secret_question", "error")
+                });
+        } catch (e) { }
+    }
+
+    client.on('change_password', change_password_handler);
+
+    function change_password_handler(input_data) {
+        try {
+            db.tx(t => {
+                input_data['hashed_password'] = password_hashing(input_data['password']);
+                const query = 'UPDATE users SET password = ${hashed_password}  WHERE username = ${username} AND answer = ${answer} RETURNING id';
+                return t.one(query, input_data);
+            })
+                .then(data => {
+                    // COMMIT
+                    client.emit("change_password", {"id": data.id})
+                })
+                .catch(error => {
+                    // ROLLBACK
+                    client.emit("change_password", {"id": -1})
+                    console.error(error)
+                });
+        } catch (e) { }
+    }
+
 })
 
-// usually this is where we try to connect to our DB.
-server.listen(process.env.PORT || 8000)
+server.listen(PORT)
+
+console.log('There server runs: ' + JSON.stringify(server.address()));
--- game-logic.js
+++ game-logic.js
@@ -1,14 +1,11 @@
-
 var io
 var gameSocket
 var gamesInSession = []
 
+const initializeGame = (sio, socket, db) => {
 
-const initializeGame = (sio, socket) => {
-
-    io = sio 
-    gameSocket = socket 
-
+    io = sio
+    gameSocket = socket
     // pushes this socket to an array which stores all the active sockets.
     gamesInSession.push(gameSocket)
 
@@ -28,20 +25,84 @@ const initializeGame = (sio, socket) => {
 
     gameSocket.on('recieved userName', recievedUserName)
 
+    // Обработчик начала игры
+    function start_game_handler(first_player_id, second_player_id) {
+        return db.tx(t => {
+            const now = new Date();
+            const entity = {
+                'id': -1,
+                'first_player_id': first_player_id,
+                'second_player_id': second_player_id,
+                'start_date': now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate(),
+                'start_time': now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds(),
+                'status': false
+            }
+            const query = 'INSERT INTO game_sessions(first_player_id, second_player_id, start_date, start_time, status)' +
+                'VALUES(${first_player_id}, ${second_player_id}, ${start_date}, ${start_time}, ${status}) RETURNING id';
+            return t.one(query, entity);
+        })
+            .then(data => {
+                return data;
+                // COMMIT
+            })
+            .catch(error => {
+                // ROLLBACK
+            });
+    }
+
+    // Обработчик окончания игры
+    function end_game_handler(start_game_handler, result ='', history ='') {
+        start_game_handler.then(_data => {
+            // Сначала меняем поле 'status' на true в таблице 'game_sessions'
+            db.tx(t => {
+                return t.query('UPDATE game_sessions SET status = true WHERE id = $1', parseInt(_data['id']));
+            })
+                .then(data => {
+                    // COMMIT
+                })
+                .catch(error => {
+                    // ROLLBACK
+                });
+            // Затем добавляем запись об окончании игры в таблицу 'game_storage'
+            db.tx(t => {
+                const now = new Date();
+                const entity = {
+                    'id': -1,
+                    'game_session_id': parseInt(_data['id']),
+                    'end_date': now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate(),
+                    'end_time': now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds(),
+                    'result': result,
+                    'history': history,
+                }
+                const query = 'INSERT INTO game_storage(game_session_id, end_date, end_time, result, history)' +
+                    'VALUES(${game_session_id}, ${end_date}, ${end_time}, ${result}, ${history}) RETURNING id';
+                return t.one(query, entity);
+            })
+                .then(data => {
+                    // COMMIT
+                })
+                .catch(error => {
+                    // ROLLBACK
+                });
+        })
+            .catch(error => {
+                console.error(error)
+            })
+    }
 }
 
 
 function playerJoinsGame(idData) {
     // A reference to the player's Socket.IO socket object
     var sock = this
-    
+
     // Look up the room ID in the Socket.IO manager object.
     var room = io.sockets.adapter.rooms[idData.gameId]
-   // console.log(room)
+    // console.log(room)
 
     // If the room exists...
     if (room === undefined) {
-        this.emit('status' , "This game session does not exist." );
+        this.emit('status', "This game session does not exist.");
         return
     }
     if (room.length < 2) {
@@ -62,12 +123,13 @@ function playerJoinsGame(idData) {
 
     } else {
         // Otherwise, send an error message back to the player.
-        this.emit('status' , "There are already 2 people playing in this room." );
+        this.emit('status', "There are already 2 people playing in this room.");
     }
 }
 
 
 function createNewGame(gameId) {
+    console.log({gameId: gameId, mySocketId: this.id})
     // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
     this.emit('createNewGame', {gameId: gameId, mySocketId: this.id});
 
@@ -78,9 +140,10 @@ function createNewGame(gameId) {
 
 function newMove(move) {
     const gameId = move.gameId
-    
+
     io.to(gameId).emit('opponent move', move);
 }
+
 
 function onDisconnect() {
     var i = gamesInSession.indexOf(gameSocket);
@@ -92,9 +155,11 @@ function requestUserName(gameId) {
     io.to(gameId).emit('give userName', this.id);
 }
 
+
 function recievedUserName(data) {
     data.socketId = this.id
     io.to(data.gameId).emit('get Opponent UserName', data);
 }
+
 
 exports.initializeGame = initializeGame
